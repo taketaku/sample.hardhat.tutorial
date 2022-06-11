be090bc99a6edecad6cef2dc63fd79eca25d500f
taketaku <vbvbsp48@gmail.com>
2022-06-11 20:11:33 +0900

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path:__dirname+'/.env'})
+
+const {ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY} = process.env
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.14",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${GOERLI_PRIVATE_KEY}`]
+    }
+  }
 };
