f2c395f51801565b691469ffb88d3ad0b7b3a658
taketaku <vbvbsp48@gmail.com>
2022-06-11 17:39:19 +0900

    chore

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract Token {
 	string public name = "My Hardhat Token";
 	string public symbol = "MHT";
@@ -14,6 +16,9 @@ contract Token {
 	}
 
 	function transfer(address to, uint256 amount) external {
+    console.log("Sender balance is %s tokens", balances[msg.sender]);
+    console.log("Trying to send %s tokens to %s", amount, to);
+
 		require(balances[msg.sender] >= amount, "Not enough tokens");
 
 		balances[msg.sender] -= amount;
